39f5420df5d98a66ff5b97c5c94396252d5662d5
rfyiamcool <rfyiamcool@163.com>
2016-09-13 11:04:18 +0800

    diy fetchman

--- extract_page/fetchman.py
+++ extract_page/fetchman.py
@@ -8,15 +8,15 @@ import redis
 import requests
 
 from config import rd, zhihu_url_key
-from task import send_task
 from log import logger
 
 
 def push_url(url):
-    status_code = send_task(url)
-    if status_code != 201:
-        logger.error("url:%s ,task not send. errcode:%s." % (url, status_code))
-    logger.info("push url %s" % (url))
+    pass
+
+
+def fetch_data(url):
+    pass
 
 
 if __name__ == '__main__':
@@ -29,11 +29,7 @@ if __name__ == '__main__':
                 time.sleep(1)
                 continue
 
-            url_list.append(url)
-            if len(url_list) > limiter:
-                push_url(url_list)
-                time.sleep(0.05)
-                url_list = []
+            fetch_data(url)
         except Exception , e:
             logger.info(traceback.format_exc())
 
